cadde6635d212b57d68ec9115874c014a4519616
zianzahid <ziamustafa2000@hotmail.com>
2024-08-13 16:21:31 -0700

    Activity 2 push

--- src/test/java/playground/Activity2.java
+++ src/test/java/playground/Activity2.java
@@ -5,18 +5,20 @@ import org.testng.annotations.Test;
 
 public class Activity2 {
 
-
-
-        @Test
-        public void testGetFullName() {
-            NameFormatter formatter = new NameFormatter();
-            String firstName = "John";
-            String lastName = "Doe";
-            String expectedFullName = "Doe, John";
-
-            String actualFullName = formatter.getFullName(firstName, lastName);
-
-            Assert.assertEquals(actualFullName, expectedFullName, "Full name format is incorrect.");
-        }
+    public String getFullName(String firstName, String lastName) {
+        return lastName + ", " + firstName;
     }
 
+    @Test
+    public void testGetFullName() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String expectedFullName = "Doe, John";
+
+
+        String actualFullName = getFullName(firstName, lastName);
+
+
+        Assert.assertEquals(actualFullName, expectedFullName, "Full name format is incorrect.");
+    }
+}
